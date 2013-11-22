3aa0e648ffeeb8d07c2c70812c3fbd35e2968979
mrthun <christian@mrthun.net>
2013-11-21 20:56:43 -0500

    Admin menu draft1

--- form2pdf.php
+++ form2pdf.php
@@ -44,122 +44,99 @@
                 $this->plugin_dir = trailingslashit( plugin_dir_path(__FILE__) );
 				$this->pdfurl = $this->plugin_dir . 'pdf/';
 				$this->templateurl = $this->plugin_dir . 'templates/';
-				$this->select_boxes = configure_select();
-                
-				// $this->debug_add('BOXES'.$this->select_boxes);
-				$this->debug_add('PATH',$this->plugin_dir . 'includes/form2pdf-options.php');
-                
-          //      if(is_admin()) {
-                    add_action('admin_init', array($this, 'register_settings'));
-                    add_action('admin_menu', array($this, 'options_page'));
-        //        } else {
-                    add_action('wp_footer', array($this,'ct_debug'));
-					add_action( 'init', array($this,'ninja_forms_create_pdf_init') );
-					
 
-
-          //      }
+                add_action('wp_footer', array($this,'ct_debug'));
+				add_action( 'init', array($this,'ninja_forms_create_pdf_init') );
             }
 			
 			
 			function ninja_forms_create_pdf_init(){
     			add_action( 'ninja_forms_before_pre_process', array($this,'ninja_forms_create_pdf' ));
-				
-				
 			}
  
-	function ninja_forms_create_pdf(){
-    	global $ninja_forms_processing,$ninja_forms_fields;
+			function ninja_forms_create_pdf(){
+    			global $ninja_forms_processing,$ninja_forms_fields;
  
-		$form_id = $ninja_forms_processing->get_form_ID();
-		$field_results = ninja_forms_get_fields_by_form_id($form_id);
-		$options = $this->get_options();
-		$file_name='';
+				$form_id = $ninja_forms_processing->get_form_ID();
+				$field_results = ninja_forms_get_fields_by_form_id($form_id);
+				$options = $Form2pdf_settings->get_options();
+				$file_name='';
 	
-		if($options['template'] !=='' && $options['form_id']==$form_id && $options['convert']==true && is_array( $field_results ) AND !empty( $field_results )) {
-					
-			$this->debug_add('AJAX SUBMIT','test');
+				if($options['template'] !=='' && $options['form_id']==$form_id && $options['convert']==true && is_array( $field_results ) AND !empty( $field_results )) {
 			
-			// Create first part of filename
-			$form_row = ninja_forms_get_form_by_id( $form_id );
-			$form_data = $form_row['data'];
-			if( isset( $form_data['form_title'] ) ){
-				$form_title = $form_data['form_title'];
-			}else{
-				$form_title .= $form_id;
-			}
-			// no spaces
-			$file_name = str_replace(' ','_',$form_title);
+					// Create first part of filename
+					$form_row = ninja_forms_get_form_by_id( $form_id );
+					$form_data = $form_row['data'];
+					if( isset( $form_data['form_title'] ) ){
+						$form_title = $form_data['form_title'];
+					}else{
+						$form_title .= $form_id;
+					}
+					// no spaces
+					$file_name = str_replace(' ','_',$form_title);
 			
-			// get the template			
-			if (is_readable($this->templateurl . $options['template'])) {
-				$file = fopen($this->templateurl . $options['template'], "r");
-				$template = fread($file, filesize($this->templateurl . $options['template']));
-				fclose($file);
-			} else {
-				$this->debug_add('FILE NOT READABLE', $this->templateurl . $options['template']);
-			}			
+					// get the template			
+					if (is_readable($this->templateurl . $options['template'])) {
+						$file = fopen($this->templateurl . $options['template'], "r");
+						$template = fread($file, filesize($this->templateurl . $options['template']));
+						fclose($file);
+					} else {
+						$this->debug_add('FILE NOT READABLE', $this->templateurl . $options['template']);
+					}			
 	
-			// cycle through the submission and do the magic
-			foreach( $field_results as $field ){
-				$field_id = $field['id'];
-				$field_type = $field['type'];
-				$field_data = $field['data'];
+					// cycle through the submission and do the magic
+					foreach( $field_results as $field ){
+						$field_id = $field['id'];
+						$field_type = $field['type'];
+						$field_data = $field['data'];
 
-				if( isset( $ninja_forms_fields[$field_type] ) ){
-					$reg_field = $ninja_forms_fields[$field_type];
-					
-					$pre_process_function = $reg_field['pre_process'];
-					if($pre_process_function != ''){
-						$arguments = array();
-						$arguments['field_id'] = $field_id;
-						$user_value = $ninja_forms_processing->get_field_value( $field_id );
-						$user_value = apply_filters( 'ninja_forms_field_pre_process_user_value', $user_value, $field_id );
-						// $this->debug_add('FIELD',$user_value);
-						$arguments['user_value'] = $user_value;
-						call_user_func_array($pre_process_function, $arguments);
+						if( isset( $ninja_forms_fields[$field_type] ) ){
+							$reg_field = $ninja_forms_fields[$field_type];					
+							$pre_process_function = $reg_field['pre_process'];
+							
+							if($pre_process_function != ''){
+								$arguments = array();
+								$arguments['field_id'] = $field_id;
+								$user_value = $ninja_forms_processing->get_field_value( $field_id );
+								$user_value = apply_filters( 'ninja_forms_field_pre_process_user_value', $user_value, $field_id );
+								$arguments['user_value'] = $user_value;
+								call_user_func_array($pre_process_function, $arguments);
 						
-						// Replace all value-tags for this field in the template
-						$template = str_replace('%v:' . $field_id . '%',$user_value,$template);
-						// Replace all label-tags for this field in the template
-						$template = str_replace('%l:' . $field_id . '%',$field_data['label'],$template);
+								// Replace all value-tags for this field in the template
+								$template = str_replace('%v:' . $field_id . '%',$user_value,$template);
+								// Replace all label-tags for this field in the template
+								$template = str_replace('%l:' . $field_id . '%',$field_data['label'],$template);
 						
 						
-						// If there was a field set with content to be added to the file name then add it now
-						if ($options['name_field'] == $field_id) {
-							$file_name .= $user_value;	
-						}
-						// Either update the filename or the url_field contents depending on settings
-						if ($options['url_field'] == $field_id) {
-							if ($options['store_versions'] == 0 && $user_value !=='' && !(empty($user_value))) {
-								// we don't store versions so we  reuse the existing filename
-								$file_name = $user_value;
-							} else {
+								// If there was a field set with content to be added to the file name then add it now
+								if ($options['name_field'] == $field_id) {
+									$file_name .= $user_value;	
+								}
+							}
+						
+							if ($options['url_field'] == $field_id) {
 								// create the new filename and update the field
 								// Add the timestamp to the filename -  only if there is a filename of course
 								$date = new DateTime();
 								$file_name .= $date->getTimestamp() . '.pdf';
 								// Now update the field in the submission
 								$ninja_forms_processing->update_field_value($field_id, "wp-content/plugins/form2pdf/pdf/" . $file_name);
-							
 							}							
 						}					
-					}				
+					}
+				
+					require_once($this->plugin_dir . "includes/dompdf/dompdf_config.inc.php");
+			
+					// Create the PDF
+					if ($file_name !== '' &&	 $template !== '') {
+						$dompdf = new DOMPDF();
+						$dompdf->load_html($template);
+						$dompdf->render();
+						$output = $dompdf->output();		
+						file_put_contents($this->pdfurl . $file_name, $output);
+					}
 				}
 			}
-			require_once($this->plugin_dir . "includes/dompdf/dompdf_config.inc.php");
-			
-			// Create the PDF
-			if ($file_name !== '' &&	 $template !== '') {
-				$dompdf = new DOMPDF();
-				$dompdf->load_html($template);
-				$dompdf->render();
-				$output = $dompdf->output();		
-				file_put_contents($this->pdfurl . $file_name, $output);
-
-			}
-		}
-	}
 			
             function debug_add($title, $content)     {
             	array_push($this->debug_out,array($title,$content));
@@ -174,237 +151,21 @@
             		echo '<br/>' . var_dump($output[1]).'<br/>';
 					
 				}
-            }
-            
-            public function install() {
-                $this->get_options();
-            }
-            
-            public function deactivate() {
-            }
-            
-            public function get_options() {
-                $options = array(
-                                 'debug' => 0,
-                                 'form_id' => 0,
-                                 'convert' => 0,
-                                 'store_versions' => 0,
-                                 'name_field' => '',
-                                 'url_field' => '',
-                                 'template' => '',
-                                 );
-                $saved = get_option($this->db_opt);
-                if(!empty($saved)) {
-                    foreach ($saved as $key => $option) {
-                        $options[$key] = $option;
-                    }
-                }
-                if($saved != $options) {
-                    update_option($this->db_opt, $options);
-                }
-                return $options;
-            }
-            
-            function options_page() {
-                add_options_page('Form2PDF Settings', 'Form2pdf', 'manage_options', 'form2pdf', array($this, 'handle_options'));
-            }
-            
-            function register_settings() {
-                register_setting('Form2pdf_Options', $this->db_opt, array($this, 'validate_options'));
-                
-
-                add_settings_section(
-                                     'form_settings',
-                                     'Form Settings',
-                                     array($this, 'form_settings_text'),
-                                     'form2pdf'
-                                     );
-                add_settings_field(
-                                   'form_id',
-                                   'Form ID',
-                                   array($this, 'form_id_input'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-                add_settings_field(
-                                   'convert',
-                                   'Convert',
-                                   array($this, 'convert_select'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-                add_settings_field(
-                                   'url_field',
-                                   'Field where URL to PDF file is stored',
-                                   array($this, 'url_field_input'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-				add_settings_field(
-                                   'store_versions',
-                                   'Store a new version with each submission',
-                                   array($this, 'version_select'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-                add_settings_field(
-                                   'name_field',
-                                   'Field that should be added to the filename',
-                                   array($this, 'name_field_input'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-				add_settings_field(
-                                   'template_field',
-                                   'Template Filename',
-                                   array($this, 'template_field_input'),
-                                   'form2pdf',
-                                   'form_settings'
-                                   );
-                
-                
-                
-
-                add_settings_section(
-                                     'debug_it',
-                                     'Debugging',
-                                     array($this, 'debug_it_text'),
-                                     'form2pdf'
-                                     );
-                
-                add_settings_field(
-                                   'debug',
-                                   'Enable Debugging',
-                                   array($this, 'debug_select'),
-                                   'form2pdf',
-                                   'debug_it'
-                                   );
-
-            }
-            
-            function debug_it_text() {
-                echo 'Enable to show debug information in the footer and the ZIP search field in the menu bar';
-                
-            }
-            
-            function debug_select() {
-                $options = $this->get_options();
-                
-                echo '<div><select name="'.$this->db_opt.'[debug]" id="debug"><option value="0"'.($options['debug'] == 0 ? ' selected="selected"' : '').'>No</option><option value="1"'.($options['debug'] == 1 ? ' selected="selected"' : '').'>Yes</option></select></div>';
-            }
-            
-            function form_settings_text() {
-                echo 'Select the form for which you want to convert submissions to PDF and the field where the PDF url is to be stored.';
-            }
-            function url_field_input() {
-                $options = $this->get_options();
-                
-                echo '<input type="text" id="url_field" name="' . $this->db_opt . '[url_field]" value="' . $options['url_field'] . '" size="25" />';
-            }
-            function convert_select() {
-                $options = $this->get_options();
-                
-                echo '<div><select name="'.$this->db_opt.'[convert]" id="convert"><option value="0"'.($options['convert'] == 0 ? ' selected="selected"' : '').'>No</option><option value="1"'.($options['convert'] == 1 ? ' selected="selected"' : '').'>Yes</option></select></div>';
-            }
-            function form_id_input() {
-            	$options = $this->get_options();
-            	echo $this->select_boxes->create_form_list($options);
-            }
-			
-            function version_select() {
-                $options = $this->get_options();
-                
-                echo '<div><select name="'.$this->db_opt.'[store_versions]" id="store_versions"><option value="0"'.($options['store_versions'] == 0 ? ' selected="selected"' : '').'>No</option><option value="1"'.($options['store_versions'] == 1 ? ' selected="selected"' : '').'>Yes</option></select></div>';
-            }
-            function name_field_input() {
-                $options = $this->get_options();
-                
-                echo '<input type="text" id="name_field" name="' . $this->db_opt . '[name_field]" value="' . $options['name_field'] . '" size="25" />';
-            }
-             function template_field_input() {
-                $options = $this->get_options();
-                
-                echo '<input type="text" id="template_field" name="' . $this->db_opt . '[template]" value="' . $options['template'] . '" size="25" />';
-            }           
-
-
-            function validate_options($input) {
-                $valid['debug'] = intval($input['debug']);
-                $valid['convert'] = intval($input['convert']);
-                $valid['template'] = $input['template'];
-                
-                // TO BE FIXED!!!!
-                $valid['url_field'] = $input['url_field'];
-                $valid['form_id'] = $input['form_id'];
-				
-				$valid['name_field'] = $input['name_field'];
-				$valid['store_versions'] = intval($input['store_versions']);
-
-                return $valid;
-            }
-            
-            public function handle_options() {
-                $settings = $this->db_opt;
-                include_once($this->plugin_dir . 'includes/form2pdf-options.php');
-            }
-            
-        }
-            
-            
+            }    
+    	}
     }
 
-	class SelectBox{
-    	public $items = array();
-    	public $defaultText = '';
-    	public $title = '';
 
-    	public function __construct($title, $default){
-        	$this->defaultText = $default;
-        	$this->title = $title;
-    	}
-
-    	public function addItem($name, $connection = NULL){
-        	$this->items[$name] = $connection;
-        	return $this;
-    	}
-
-    	public function toJSON(){
-        	return json_encode($this);
-    	}
-		
-		public function create_form_list($options) {
-			$options;
-			$the_list='<ul id="forms"><li><p>Select the form from which you want to create PDF files.</p><select data-placeholder="Select a form">';
-			$all_forms = ninja_forms_get_all_forms();
-			foreach($all_forms as $theform) {
-				$the_list .= '<option data-connection="' 
-				. $theform['id'] . '" value="' 
-				. $theform	['id'] 
-				. '"'.($options['form_id'] == $theform['id'] ? ' selected="selected"' : '').'>'
-				. $theform['data']['form_title'] . '</options>';
-			}
-			$the_list .= '</select></li></ul>';
-			
-			return $the_list;
-		}
-		
-	}
-	
-				
-	function configure_select() {
-		$select = new SelectBox('What would you like to purchase?','Choose a product category');
-		$select	->addItem('Phones','phoneSelect')
-              			->addItem('Notebooks','notebookSelect')
-              			->addItem('Tablets','tabletSelect');
-		return $select;
-	}
-	
-
+	include_once(trailingslashit( plugin_dir_path(__FILE__) ) . 'includes/form2pdf-settings.php');
+	$Form2pdf_settings = new Form2pdf_settings();
+	if($Form2pdf_settings) {  	
+        register_activation_hook( __FILE__, array(&$Form2pdf_settings, 'install'));
+        // Adds an ajax actions.
+    }
 
     $Form2pdf = new Form2pdf();
     
     if($Form2pdf) {  	
-        register_activation_hook( __FILE__, array(&$Form2pdf, 'install'));
         // Adds an ajax actions.
     }
     
---
+++ includes/form2pdf-settings.php
@@ -0,0 +1,154 @@
+<?php
+    interface wp_settings
+    {
+    	function install();
+		function settings_page();
+		function register_settings();
+		function validate_settings($input);
+		function handle_settings();
+		function get_settings();
+    }
+	
+    if(!class_exists('Form2pdf_settings')) {
+	
+	class Form2pdf_settings implements wp_settings
+	{
+		
+		var $db_opt = 'Form2pdf_Options';
+		var $debug_out = array();
+		
+		public function __construct()
+		{
+			if(is_admin()) {
+            	add_action('admin_init', array($this, 'register_settings'));
+                add_action('admin_menu', array($this, 'settings_page'));
+        	}
+			add_action('admin_footer', array($this,'ct_debug'));
+		}
+		public function install()
+		{
+			$this->get_settings();
+		}
+		
+		public function get_settings()
+		{
+
+			// get the saved settings from Wordpress
+            $settings = get_option($this->db_opt);
+			// Get all form IDs because they are the keys in our $saved array
+			$all_forms = ninja_forms_get_all_forms();
+			foreach($all_forms as $one_form)
+			{
+				// New form? Add an entry to the settings list
+				$form_id = $one_form['id'];
+				if ($form_id>0) {
+					if ($settings) {
+						if (!array_key_exists ($form_id,$settings)) 
+						{
+							$settings[$one_form['id']] = array(
+							'form_id' => $one_form['id'],
+							'convert' => false, 
+							'url_field' => 2, 
+							'template' =>'', 
+							'form_name' => $one_form['data']['form_title']
+					     	);
+							// write the settings back to the database
+							update_option($this->db_opt, $settings);
+						}
+					} else {
+						$settings[$one_form['id']] = array(
+							'form_id' => $one_form['id'],
+							'convert' => false, 
+							'url_field' => 5, 
+							'template' =>'template', 
+							'form_name' => $one_form['data']['form_title']
+					     );
+						// write the settings back to the database
+						update_option($this->db_opt, $settings);
+					}					
+					
+				}
+			}
+            return $settings;			
+		}
+		
+		public function settings_page()
+		{
+			add_options_page('Form2PDF Settings', 
+				'Form2pdf', 
+				'manage_options', 
+				'form2pdf', 
+				array($this, 'handle_settings'));
+		}
+		
+		public function handle_settings() 
+		{
+        	$plugin_dir = trailingslashit( plugin_dir_path(__FILE__) );
+			echo $include_dir;
+            include_once($plugin_dir . 'form2pdf-options.php');
+        }
+		
+		public function register_settings() {
+			$settings = $this->get_settings();
+			// delete_option( $this->db_opt); 
+			$this->debug_add('BEFORE',$settings);
+         	register_setting('Form2pdf_Options', $this->db_opt, array($this, 'validate_settings'));
+			
+			add_settings_section(
+            	'form_settings',
+                'Form Settings',
+                 array($this, 'form_settings_text'),
+                 'form2pdf'
+            );
+			
+			// This calls the routine that will add all the fields
+            add_settings_field(
+                'forms',
+                'Forms',
+                array($this, 'output_form_fields'),
+                'form2pdf',
+                'form_settings'
+            );		
+		 }
+		 
+		public function validate_settings($input) 
+		 {
+		 	$valid = $input;
+		 	return $valid;
+		 }
+		 
+		 public function form_settings_text() {
+		 	echo 'Select for each form how and if you want the PDF to be created';
+		 }
+		 
+		 public function output_form_fields()
+		 {
+		 	// Output all formfield - one set per form
+		 	$settings = $this->get_settings();
+			var_dump($settings);
+			foreach($settings as $setting) {
+				echo '<div><br/>' . $setting['form_name'] . '<br/>';
+			echo 'Field where the PDF url is stored:<input type="text" id="url_field" name="' . $this->db_opt. '[' .$setting['form_id']. '][url_field]" value="' . $setting['url_field'] . '" size="25" /></div>';
+			echo 'Template name:<input type="text" id="template" name="' . $this->db_opt. '[' .$setting['form_id']. '][template]" value="' . $setting['template'] . '" size="25" />';
+			//	echo '<input type="checkbox" id="template" name="' . $this->db_opt. '[' .$settings['id']. '][template]" value="' . $settings['template'] . '"';
+			}
+			
+		 }	
+		 
+		    function debug_add($title, $content)     {
+            	array_push($this->debug_out,array($title,$content));
+            }   
+			 
+            function ct_debug() {
+            	// var_dump($this->debug_out);
+            	foreach( $this->debug_out as $output ){
+            		if(gettype($output[0])=='string') {
+            			echo '<br/>' . $output[0] . '<br/>';
+            		}
+            		echo '<br/>' . var_dump($output[1]).'<br/>';
+					
+				}
+            } 	 
+	}
+}
+?>
